31a0958abca0a450d69d8dcb244c6bbab71afd90
Ioana Mot <mmioana94@gmail.com>
2018-05-29 20:58:20 +0300

    Added test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -60,15 +63,46 @@ describe('Server', () => {
                     });
                 });
         });
+    });
 
+    describe('GET /todos', () => {
         it('should get all todos', (done) => {
             request(app)
                 .get('/todos')
                 .expect(200)
                 .expect((res) => {
-                    expect(res.body.todos.length).toBe(todos.length);
+                expect(res.body.todos.length).toBe(todos.length);
+        })
+        .end(done);
+        });
+    });
+
+    describe('GET /todos/:id', () => {
+        it('should get valid todo', (done) => {
+
+            request(app)
+                .get(`/todos/${todos[0]._id.toHexString()}`)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.text).toBe(todos[0].text);
                 })
                 .end(done);
+        });
+
+        it('should get no todo', (done) => {
+            var id = new ObjectID().toHexString();
+            request(app)
+                .get(`/todos/${id}`)
+                .expect(404)
+                .end(done);
+        });
+
+        it('should get invalid id', (done) => {
+            var id = new ObjectID().toHexString() + 'abc';
+                request(app)
+                    .get(`/todos/${id}`)
+                    .expect(400)
+                    .end(done);
         });
     });
 });
