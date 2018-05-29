118351c4547bdee62e36dc6e140c5012b393411c
Ioana Mot <mmioana94@gmail.com>
2018-05-29 19:44:45 +0300

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
 
 // GET /todos/:id
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
 });
