ca18a1a614b5773e6af4e2b220c1d5112b777644
Ioana Mot <mmioana94@gmail.com>
2018-05-29 20:27:55 +0300

    Added test case for GET route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('Server', () => {
@@ -25,7 +33,7 @@ describe('Server', () => {
                         return done(err);
                     }
 
-                    Todo.find().then((todos) => {
+                    Todo.find({text}).then((todos) => {
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
                         done();
@@ -45,12 +53,22 @@ describe('Server', () => {
                         return done(err);
                     }
                     Todo.find().then((todos) => {
-                        expect(todos.length).toBe(0);
+                        expect(todos.length).toBe(todos.length);
                         done();
                     }, (err) => {
                         done(err);
                     });
                 });
-        })
+        });
+
+        it('should get all todos', (done) => {
+            request(app)
+                .get('/todos')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todos.length).toBe(todos.length);
+                })
+                .end(done);
+        });
     });
 });
