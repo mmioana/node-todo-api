612d89b459f39b60f8d58670ef33694da7bef27c
Ioana Mot <mmioana94@gmail.com>
2018-05-29 20:42:59 +0300

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -20,7 +21,6 @@ app.post('/todos', (req, res) => {
     });
 });
 
-// GET /todos/:id
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -29,6 +29,26 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 });
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    let todoId = req.params.id;
+    Todo.findById(todoId)
+        .then((doc) => {
+            if (!doc) {
+                res.status(404).send('');
+            }
+            res.send(doc);
+        })
+        .catch((e) => {
+            if (!ObjectID.isValid(todoId)) {
+                res.status(400).send('Invalid id.');
+            }
+            res.send(e);
+        });
+})
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
