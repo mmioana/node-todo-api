737f94fa6c329126560d12c03cc46c94ae6eb884
Ioana Mot <mmioana94@gmail.com>
2018-05-30 09:25:51 +0300

    Added config for heroku deployment

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 let app = express();
-let port = process.env.PORT || 8000;
+let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
